7c24d02db4c4cd750222bf714646bfe58293e4a6
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-09 22:42:03 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,19 @@
+package recursion;
+//by stack falling return type
+public class prime {
+	public static String primenrs(int num,int temp) {
+		if(temp==(int)Math.sqrt(num)) {
+			return "the number is prime";
+		}
+		if(num%temp!=0) {
+			return primenrs(num,temp+1);
+		}
+		else {
+			return "the number is not prime";
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(primenrs(53,2));
+	}
+    
+}
