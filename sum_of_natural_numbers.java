1ac9b6d7fdd8376ebf7f93ff76dce7235ff72c0e
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-09 22:38:10 +0530

    Create sum_of_natural_numbers.java

---
+++ sum_of_natural_numbers.java
@@ -0,0 +1,29 @@
+package recursion;
+
+public class sum_of_natural_numbers 
+{
+	static void sum(int start,int end,int result) {
+		
+		if(start==end) {
+			result+=start;
+			System.out.println(result);
+			return;
+		}
+		result+=start;
+		sum(start+1,end,result);
+	}
+	static int sumOfNumber(int start,int end) {
+		if(start==end) {
+			return start;
+		}
+		return start+sumOfNumber(start+1,end);
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		sum(1,10,0);
+		System.out.println(sumOfNumber(1,15));
+	}
+
+}
