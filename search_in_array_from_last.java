2e17f59d06af9e1020b2f75901e5cbd3563d56bc
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-09 22:50:55 +0530

    Create search_in_array_from_last.java

---
+++ search_in_array_from_last.java
@@ -0,0 +1,27 @@
+package recursion;
+import java.util.*;
+public class search_from_last {
+
+	static int search(int a[],int num,int start) {
+		if(start<0) {
+			return -1;
+		}
+		if(a[start]==num) {
+			return 1;
+		}
+		return search(a,num,start-1);
+		
+	}
+	public static void main(String[] args) {
+		int [] array= {1,2,3,4,5,6,7,8,9,10};
+		int res=search(array,1,0);
+		if(res==1) {
+			System.out.println("PRESENT");
+		}
+		else {
+			System.out.println("NOT PRESENT");
+		}
+
+	}
+
+}
