9771b993094d769b9cc7b1294aa49f0d80f72c1e
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-11 01:16:34 +0530

    Update 6_Armstrong_Number.java

--- 6_Armstrong_Number.java
+++ 6_Armstrong_Number.java
@@ -5,20 +5,29 @@
 // 1^3 + 5^3 + 3^3 = 153
 
 
-//by stack falling return type
-public class Armstrong_Number {
-	public static int arms(int num) {
-		if(num==0) {
-			return 0;
-		}
-		int res=arms(num/10);
-		res=(int)(res+Math.pow(num%10, 3));
-		return res;
-	}
-	public static void main(String[] args) {
-		int num =153;
-		System.out.println(num==arms(num)?"Armstrong":"not an Armstrong");
-		
-	}
 
+public class ArmstrongNumberRecursive {
+    public static void main(String[] args) {
+        int number = 153;
+        if (isArmstrong(number, countDigits(number)))
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+
+    // Recursive function to check if a number is Armstrong or not
+    public static boolean isArmstrong(int number, int numDigits) {
+        if (number == 0)
+            return true;
+        int digit = number % 10;
+        int power = (int) Math.pow(digit, numDigits);
+        return power + isArmstrong(number / 10, numDigits);
+    }
+
+    // Recursive function to count the number of digits in a number
+    public static int countDigits(int number) {
+        if (number == 0)
+            return 0;
+        return 1 + countDigits(number / 10);
+    }
 }
