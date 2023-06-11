3b58622b9bc8d7f3bf782ed43e535cd78cf092d9
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-11 17:46:00 +0530

    Update 2_Count Zero.java

--- 2_Count Zero.java
+++ 2_Count Zero.java
@@ -5,20 +5,31 @@ package recursion;
 
 public class count_zero {
 
-	static int no_of_zero(int num) {
+	//	1. void return type - during stack building
+	public static void no_of_zero1(int num, int count) {
+		if(num == 0) {
+			System.out.println("Count of zeroes : "+count);
+			return;
+		}
+		if(num%10 == 0)count ++;
+		no_of_zero1(num/10,count);
+	}
+//	2. int return type - during stack falling
+	static int no_of_zero2(int num) {
 		
 		if(num==0) {
 			return 0;
 		}
 		if(num%10==0) {
-			return 1+no_of_zero(num/10);
+			return 1+no_of_zero2(num/10);
 		}
-		return no_of_zero(num/10);
+		return no_of_zero2(num/10);
 	}
 
 	public static void main(String[] args) 
 	{
-		System.out.println(no_of_zero(10040));
+		no_of_zero1(10040);
+		System.out.println(no_of_zero2(10040));
 	
 	}
 
