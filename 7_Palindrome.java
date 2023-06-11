ed1dd199052bdaa8faaf1ec2f694ff7dc748d100
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-11 17:53:31 +0530

    Update 7_Palindrome.java

--- 7_Palindrome.java
+++ 7_Palindrome.java
@@ -3,6 +3,20 @@ package recursion;
 import java.util.Scanner;
 public class Palindrome 
 {
+	//	1. void return type - during stack building
+	public static void palindromeCheck1(String str, int index) {
+		if(index == str.length()/2+1) {
+			System.out.println(str+ " : Palindrome");
+			return;
+		}
+		if(Character.compare(str.charAt(index), str.charAt(str.length()-1-index)) != 0){
+			System.out.println(str + " : Not palindrome");
+			return;
+		}
+		palindromeCheck1(str,index+1);
+	}
+//	2. String return type - during stack falling
+	
 	static boolean checkPalindrome(String str, int s, int e) 
     { 
         if (s == e)    // If there is only one character 
@@ -18,6 +32,7 @@ public class Palindrome
     }   
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		palindromeCheck1("abbawwa",0);
 		System.out.println("ENTER A STRING:");
 		String s=sc.nextLine();
 		boolean x=checkPalindrome(s,0,s.length()-1)?true:false;
