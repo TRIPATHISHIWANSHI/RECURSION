ed4d35bb7bcfc6b6c463c5388ff31244366230e1
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-11 17:43:53 +0530

    Update 1_CALCULATING POWER.java

--- 1_CALCULATING POWER.java
+++ 1_CALCULATING POWER.java
@@ -4,18 +4,29 @@ package recursion;
 
 public class power 
 {
-	public static double calculatePower(double base, int power) {
+	//	1. void return type - during stack building
+	public static void calculatePower1(int num, int pow, int res) {
+		if(pow == 0) {
+			System.out.println("Calculated Power is : "+res);
+			return;
+		}
+		res*=num;
+		calculatePower1(num, pow-1, res);
+	}
+//	2. int return type - during stack falling
+	public static double calculatePower2(double base, int power) {
         if (power == 0) {
             return 1;  
         } else if (power < 0) {
-            return 1 / calculatePower(base, -power);  
+            return 1 / calculatePower2(base, -power);  
         } else {
-            return base * calculatePower(base, power - 1);
+            return base * calculatePower2(base, power - 1);
         }
     }
+	
 	public static void main(String[] args) {
-		System.out.println(calculatePower(2,5));
-		
+		System.out.println(calculatePower2(2,5));
+		calculatePower1(2,5)
 	}
 
 }
