671f2ba44723fcfb949ebc85b8dcf0628e9dd90c
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-11 10:12:50 +0530

    Update 6_Armstrong_Number.java

--- 6_Armstrong_Number.java
+++ 6_Armstrong_Number.java
@@ -1,33 +1,38 @@
-//6. Check the Given Number is Armstrong or Not?
-
-// 153
-
-// 1^3 + 5^3 + 3^3 = 153
-
-
+package recursion;
 
 public class ArmstrongNumberRecursive {
+    public static boolean isArmstrong(int number) {
+        int numDigits = countDigits(number);
+        int sum = calculateSumOfPowers(number, numDigits);
+        
+        return number == sum;
+    }
+    
+    private static int countDigits(int number) {
+        if (number == 0) {
+            return 0;
+        } else {
+            return 1 + countDigits(number / 10);
+        }
+    }
+    
+    private static int calculateSumOfPowers(int number, int numDigits) {
+        if (number == 0) {
+            return 0;
+        }
+        
+        int digit = number % 10;
+        int poweredDigit = (int) Math.pow(digit, numDigits);
+        
+        return poweredDigit + calculateSumOfPowers(number / 10, numDigits);
+    }
+    
     public static void main(String[] args) {
         int number = 153;
-        if (isArmstrong(number, countDigits(number)))
+        if (isArmstrong(number)) {
             System.out.println(number + " is an Armstrong number.");
-        else
+        } else {
             System.out.println(number + " is not an Armstrong number.");
-    }
-
-    // Recursive function to check if a number is Armstrong or not
-    public static boolean isArmstrong(int number, int numDigits) {
-        if (number == 0)
-            return true;
-        int digit = number % 10;
-        int power = (int) Math.pow(digit, numDigits);
-        return power + isArmstrong(number / 10, numDigits);
-    }
-
-    // Recursive function to count the number of digits in a number
-    public static int countDigits(int number) {
-        if (number == 0)
-            return 0;
-        return 1 + countDigits(number / 10);
+        }
     }
 }
