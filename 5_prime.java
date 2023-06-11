81d0e22f79b0214c514ad03bdfafc23fc24623ea
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-06-11 17:50:28 +0530

    Update 5_prime.java

--- 5_prime.java
+++ 5_prime.java
@@ -2,6 +2,19 @@
 package recursion;
 //by stack falling return type
 public class prime {
+	//	1. void return type - during stack building
+	public static void primeCheck1(int num, int i) {
+		if(i==num) {
+			System.out.println(num+" is Prime");
+			return;
+		}
+		if(num%i==0) {
+			System.out.println(num+" is not prime");
+			return;
+		}
+		primeCheck1(num,i+1);
+	}
+//	2. String return type - during stack falling
 	public static String primenrs(int num,int temp) {
 		if(temp==(int)Math.sqrt(num)) {
 			return "the number is prime";
@@ -15,6 +28,7 @@ public class prime {
 	}
 	public static void main(String[] args) {
 		System.out.println(primenrs(53,2));
+		primeCheck1(46, 2);
 	}
     
 }
